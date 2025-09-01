057b22da5ef81d397fb26167da1dee101ad94839
Zama Khan Mohammed <mohammedzamakhan@gmail.com>
2025-09-01 16:43:11 -0500

    feat: use fontless in astro web app

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,10 +1,11 @@
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import { fontless } from 'fontless';
 
 export default defineConfig({
   integrations: [react()],
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), fontless()],
   },
 });
